2cf246358a55a47a2320925d7c6dabdb0838b979
Sempr <iamsempr@gmail.com>
2017-12-19 17:17:13 +0800

    MOD: add stream method

--- rpc/ipsvc_cli.py
+++ rpc/ipsvc_cli.py
@@ -1,7 +1,6 @@
 import sys
-
-sys.path.insert(0, ".")
-sys.path.insert(0, "../")
+import os.path
+sys.path.insert(0,  os.path.abspath("."))
 
 import grpc
 from rpc import ipsvc_pb2_grpc
@@ -11,9 +10,16 @@ from rpc import ipsvc_pb2
 def run():
     channel = grpc.insecure_channel('127.0.0.1:50051')
     stub = ipsvc_pb2_grpc.IPSVCStub(channel)
+
     req = ipsvc_pb2.IPsRequest(ips=[ipsvc_pb2.IPRequest(ip="{i}.{i}.{i}.{i}".format(i=i)) for i in range(256)])
     response = stub.IPSQuery(req)
     for x in response.ipr:
+        print(x.ip, x.city, x.loc)
+    
+    val = [ipsvc_pb2.IPRequest(ip=".".join([str(i)]*4)) for i in range(255)]
+
+    resp = stub.IPStreamQuery(iter(val))
+    for x in resp:
         print(x.ip, x.city, x.loc)
 
 
