9e7285f10a9a6280be33fe6603503f25ee0d01b8
Sempr <iamsempr@gmail.com>
2026-01-27 13:07:35 +0800

    MOD: upgrade libs and change data path

--- run_download.sh
+++ run_download.sh
@@ -4,9 +4,10 @@ set -xe
 dir="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 cd $dir
 
-mkdir data
+mkdir -p data
 pushd data
 
-curl -OL https://github.com/lionsoul2014/ip2region/raw/master/data/ip2region.xdb
+curl -OL https://github.com/lionsoul2014/ip2region/raw/master/data/ip2region_v4.xdb
+mv ip2region_v4.xdb ip2region.xdb
 
 popd
